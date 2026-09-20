817518a52e3365679f39d79343f1633559d1a542
II-EMMA <emma.ahmedera@gmail.com>
2026-09-20 15:21:58 +0300

    Large Class Method.

--- bloaters.js
+++ bloaters.js
@@ -1,3 +1,5 @@
+// Bloaters ==>
+// Smell Code #1
 class UserReportManager {
   sendUserReport(user) {
     // 1. حساب متوسط تقييمات المستخدم
@@ -54,5 +56,88 @@ class UserReportManager {
     const averageScore = this.calculateAverageScore(user.scores);
     const status = this.determineStatus(averageScore);
     this.sendEmail(user, status, averageScore);
+  }
+}
+
+// Smell Code #2
+class Product {
+  constructor(
+    name,
+    price,
+    ratingValue,
+    ratingCount,
+    discountPercentage,
+    isDiscountActive,
+  ) {
+    // بيانات المنتج
+    this.name = name;
+    this.price = price;
+
+    // بيانات التقييم
+    this.ratingValue = ratingValue;
+    this.ratingCount = ratingCount;
+
+    // بيانات الخصم
+    this.discountPercentage = discountPercentage;
+    this.isDiscountActive = isDiscountActive;
+  }
+
+  // دالة متعلقة بالتقييم
+  getRatingSummary() {
+    return `${this.ratingValue} / 5 (${this.ratingCount} reviews)`;
+  }
+
+  // دالة متعلقة بالخصم
+  getDiscountedPrice() {
+    if (this.isDiscountActive) {
+      return this.price - this.price * (this.discountPercentage / 100);
+    }
+    return this.price;
+  }
+}
+
+// Large Class
+// 1. كلاس مستقل للتقييم
+class Rating {
+  constructor(value, count) {
+    this.value = value;
+    this.count = count;
+  }
+
+  getSummary() {
+    return `${this.value} / 5 (${this.count} reviews)`;
+  }
+}
+
+// 2. كلاس مستقل للخصم
+class Discount {
+  constructor(percentage, isActive) {
+    this.percentage = percentage;
+    this.isActive = isActive;
+  }
+
+  calculateDiscountedPrice(basePrice) {
+    if (this.isActive) {
+      return basePrice - basePrice * (this.percentage / 100);
+    }
+    return basePrice;
+  }
+}
+
+// 3. كلاس Product النظيف بيجمع الكائنات سوا
+class Product {
+  constructor(name, price, rating, discount) {
+    this.name = name;
+    this.price = price;
+    this.rating = rating; // Instance of Rating
+    this.discount = discount; // Instance of Discount
+  }
+
+  getRatingSummary() {
+    return this.rating.getSummary();
+  }
+
+  getFinalPrice() {
+    return this.discount.calculateDiscountedPrice(this.price);
   }
 }
